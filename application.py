30852ab36052d0ebf3b3cb6f9b7d8dde8e2ad194
Tianwei Zhang <zhang13music@outlook.com>
2019-04-18 18:34:22 -0500

    update about page and nav bar

--- application.py
+++ application.py
@@ -35,6 +35,9 @@ STUDENT = 'student'
 def index():
     return render_template('index.html')
 
+@application.route('/about')
+def about():
+    return render_template("about.html")
 
 @application.route('/signup', methods=['GET', 'POST'])
 def signup():
